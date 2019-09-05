4f784e9b052ae51bdb63be7fa0db2d6d7eb67cec
vinod1209 <vinodpatil1209@gmail.com>
2019-09-05 18:07:51 +0530

    Added new ArrayList class

---
+++ ArrayLisiDemo1.java
@@ -0,0 +1,21 @@
+package com.vp.collection;
+
+import java.util.ArrayList;
+
+public class ArrayLisiDemo1 {
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(6);
+		al.add(9);
+		al.add(6);
+		al.add(null);
+		al.add("v");
+		al.add("c");
+		al.add("s");
+		al.add(null);
+		al.add("v");
+		
+		System.out.println(al);
+	}
+
+}
